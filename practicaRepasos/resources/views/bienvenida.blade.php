e18dd58d635158bc9e1815ca28d18a969b93aaad
nancyjazmin <122044805@upq.edu.mx>
2024-10-18 18:53:25 -0600

    Paso3
    Creación de la página principal

---
+++ practicaRepasos/resources/views/bienvenida.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Inicio</title>
+    @vite(['resources/js/app.js'])
+    <style>
+        body, html {
+            height: 100%;
+        }
+        .full-height {
+            height: 100vh;
+        }
+    </style>
+
+<div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
+
+    <h1 class="display-1"> Bienvenido al Convertidor 
+        <br>
+        de 
+        <br>
+        Nancy Jazmín Sánchez Hernández 
+        </h1>
+
+        <p>Presiona el botón para iniciar....</p>
+
+    <a href="/convertidor" class="btn btn-primary"> Ir al Conventidor </a>
+     
+
+</div>
+
+</head>
+<body>
+
+</body>
+</html>
--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('bienvenida');
 });
+
+Route:: view('/','bienvenida')->name('rutabienvenida');
